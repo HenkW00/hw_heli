e291cf27cbe2867f38af7ee5c5c1620b535dff88
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-22 05:40:02 +0200

    Changed version handlers

--- heli_server.lua
+++ heli_server.lua
@@ -1,32 +1,111 @@
-local resourceName = "hw_heli1"
-local scriptVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
+local curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+local isBought = true
+local resourceName = 'hw_heli'
+local authPrinted = false
 
-AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
+CreateThread(function()
+    while true do
+        PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_heli/releases/latest", CheckVersion, "GET")
+        Wait(3500000)
+    end
+end)
+
+CheckVersion = function(err, responseText, headers)
+    local repoVersion, repoURL, repoBody = GetRepoInformations()
+
+    CreateThread(function()
+        if curVersion ~= repoVersion then
+            Wait(4000)
+            print("^0[^3WARNING^0] ^5" .. resourceName .. "^0 is ^1NOT ^0up to date!")
+            print("^0[^3WARNING^0] Your Version: ^2" .. curVersion .. "^0")
+            print("^0[^3WARNING^0] Latest Version: ^2" .. repoVersion .. "^0")
+            print("^0[^3WARNING^0] Get the latest Version from: ^2" .. repoURL .. "^0")
+            print("^0[^3WARNING^0] Changelog:^0")
+            print("^1" .. repoBody .. "^0")
+        else
+            Wait(4000)
+            print("^0[^2INFO^0] ^5" .. resourceName .. "^0 is up to date! (^2" .. curVersion .. "^0)")
+        end
+    end)
+end
+
+GetRepoInformations = function()
+    local repoVersion, repoURL, repoBody = nil, nil, nil
+
+    PerformHttpRequest("https://api.github.com/repos/HenkW00/hw_heli/releases/latest", function(err, response, headers)
+        if err == 200 then
+            local data = json.decode(response)
+
+            repoVersion = data.tag_name
+            repoURL = data.html_url
+            repoBody = data.body
+        else
+            repoVersion = curVersion
+            repoURL = "https://github.com/HenkW00/hw_heli"
+            print('^0[^3WARNING^0] Could ^1NOT^0 verify latest version from ^5github^0!')
+        end
+    end, "GET")
+
+    repeat
+        Wait(50)
+    until (repoVersion and repoURL and repoBody)
+
+    return repoVersion, repoURL, repoBody
+end
+
+AddEventHandler('onResourceStart', function(startedResourceName)
+    local folderName = string.match(GetResourcePath(startedResourceName), "/([^/]+)$")
+    if not isBought or (folderName ~= resourceName) then
+        if not authPrinted then
+            while true do
+                Citizen.Wait(3000)
+                print('^7=====================================================================')
+                print('^0[^2INFO^0] ^1You are not allowed to change the name of this resource!')
+                print('^0[^2INFO^0] ^1Please rename the script to: ^3' .. resourceName)
+                print('^7=====================================================================')
+                authPrinted = true
+            end
+        end
         return
     end
+
     print('^7> ================================================================')
-    print('^7> ^5[HW Scripts] ^7| ^3' .. resourceName .. ' ^2has been started.') 
-    print('^7> ^5[HW Scripts] ^7| ^2Current version: ^3' .. scriptVersion)
+    print('^7> ^5[HW Scripts] ^7| ^3' .. startedResourceName .. ' ^2has been started.') 
+    print('^7> ^5[HW Scripts] ^7| ^2Current version: ^3' .. curVersion)
     print('^7> ^5[HW Scripts] ^7| ^6Made by HW Development')
     print('^7> ^5[HW Scripts] ^7| ^8Creator: ^3Henk W')
-    print('^7> ^5[HW Scripts] ^7| ^4Shop Link: ^3hw-scripts-store.tebex.io/')
+    print('^7> ^5[HW Scripts] ^7| ^4Github: ^3https://github.com/HenkW00')
     print('^7> ^5[HW Scripts] ^7| ^4Discord Server Link: ^3https://discord.gg/buqhWxVYkQ')
     print('^7> ================================================================')
 end)
 
-AddEventHandler('onResourceStop', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
+AddEventHandler('onResourceStop', function(stoppedResourceName)
+    local folderName = string.match(GetResourcePath(stoppedResourceName), "/([^/]+)$")
+    if not isBought or (folderName ~= resourceName) then
+        if not authPrinted then
+            while true do
+                Citizen.Wait(3000)
+                print('^7=====================================================================')
+                print('^0[^2INFO^0] ^1You are not allowed to change the name of this resource!')
+                print('^0[^2INFO^0] ^1Please rename the script to: ^3' .. resourceName)
+                print('^7=====================================================================')
+                authPrinted = true
+            end
+        end
         return
     end
-    print('^7> ===========================================')
-    print('^7> ^5[HW Scripts] ^7| ^3' .. resourceName .. ' ^1has been stopped.')
+
+    print('^7> ================================================================')
+    print('^7> ^5[HW Scripts] ^7| ^3' .. stoppedResourceName .. ' ^1has been stopped.') 
+    print('^7> ^5[HW Scripts] ^7| ^2Current version: ^3' .. curVersion)
     print('^7> ^5[HW Scripts] ^7| ^6Made by HW Development')
     print('^7> ^5[HW Scripts] ^7| ^8Creator: ^3Henk W')
-    print('^7> ===========================================')
+    print('^7> ^5[HW Scripts] ^7| ^4Github: ^3https://github.com/HenkW00')
+    print('^7> ^5[HW Scripts] ^7| ^4Discord Server Link: ^3https://discord.gg/buqhWxVYkQ')
+    print('^7> ================================================================')
 end)
 
-local discordWebhook = "https://discord.com/api/webhooks/1187745655242903685/rguQtJJN1QgnaPm5xGKOMqHePhfX6hhFofaSpWIphhtwH5bLAG1dx5RxJrj-BxiFMjaf"
+local dw = "https://discord.com/api/webhooks/1187745655242903685/rguQtJJN1QgnaPm5xGKOMqHePhfX6hhFofaSpWIphhtwH5bLAG1dx5RxJrj-BxiFMjaf"
 
 function sendDiscordEmbed(embed)
     local serverIP = GetConvar("sv_hostname", "Unknown")
@@ -34,21 +113,20 @@ function sendDiscordEmbed(embed)
     embed.description = embed.description .. "\nServer Name: `" .. serverIP .. "`"
 
     local discordPayload = json.encode({embeds = {embed}})
-    PerformHttpRequest(discordWebhook, function(err, text, headers) end, 'POST', discordPayload, { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(dw, function(err, text, headers) end, 'POST', discordPayload, { ['Content-Type'] = 'application/json' })
 end
 
-AddEventHandler('onResourceStart', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
+AddEventHandler('onResourceStart', function(resourceStartedName)
+    if (GetCurrentResourceName() ~= resourceStartedName) then
         return
     end
 
-    local scriptName = GetCurrentResourceName() 
 
     local embed = {
         title = "Resource Started",
-        description = string.format("**%s** has been started.", scriptName), 
+        description = string.format("**%s** has been started.", resourceStartedName), 
         fields = {
-            {name = "Current version", value = scriptVersion},
+            {name = "Current version", value = curVersion},
             {name = "Discord Server Link", value = "[Discord Server](https://discord.com/invite/buqhWxVYkQ)"}
         },
         footer = {
@@ -60,16 +138,14 @@ AddEventHandler('onResourceStart', function(resourceName)
     sendDiscordEmbed(embed)
 end)
 
-AddEventHandler('onResourceStop', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
+AddEventHandler('onResourceStop', function(resourceStoppedName)
+    if (GetCurrentResourceName() ~= resourceStoppedName) then
         return
     end
 
-    local scriptName = GetCurrentResourceName() 
-
     local embed = {
         title = "Resource Stopped",
-        description = string.format("**%s** has been stopped.", scriptName),
+        description = string.format("**%s** has been stopped.", resourceStoppedName),
         footer = {
             text = "HW Scripts | Logs"
         },
@@ -77,68 +153,4 @@ AddEventHandler('onResourceStop', function(resourceName)
     }
 
     sendDiscordEmbed(embed)
-end)
-
-
-
-
-
-RegisterServerEvent('heli:forward.spotlight')
-AddEventHandler('heli:forward.spotlight', function(state)
-	local serverID = source
-	TriggerClientEvent('heli:forward.spotlight', -1, serverID, state)
-end)
-
-RegisterServerEvent('heli:tracking.spotlight')
-AddEventHandler('heli:tracking.spotlight', function(target_netID, target_plate, targetposx, targetposy, targetposz)
-	local serverID = source
-	TriggerClientEvent('heli:Tspotlight', -1, serverID, target_netID, target_plate, targetposx, targetposy, targetposz)
-end)
-
-RegisterServerEvent('heli:tracking.spotlight.toggle')
-AddEventHandler('heli:tracking.spotlight.toggle', function()
-	local serverID = source
-	TriggerClientEvent('heli:Tspotlight.toggle', -1, serverID)
-end)
-
-RegisterServerEvent('heli:pause.tracking.spotlight')
-AddEventHandler('heli:pause.tracking.spotlight', function(pause_Tspotlight)
-	local serverID = source
-	TriggerClientEvent('heli:pause.Tspotlight', -1, serverID, pause_Tspotlight)
-end)
-
-RegisterServerEvent('heli:manual.spotlight')
-AddEventHandler('heli:manual.spotlight', function()
-	local serverID = source
-	TriggerClientEvent('heli:Mspotlight', -1, serverID)
-end)
-
-RegisterServerEvent('heli:manual.spotlight.toggle')
-AddEventHandler('heli:manual.spotlight.toggle', function()
-	local serverID = source
-	TriggerClientEvent('heli:Mspotlight.toggle', -1, serverID)
-end)
-
-RegisterServerEvent('heli:light.up')
-AddEventHandler('heli:light.up', function()
-	local serverID = source
-	TriggerClientEvent('heli:light.up', -1, serverID)
-end)
-
-RegisterServerEvent('heli:light.down')
-AddEventHandler('heli:light.down', function()
-	local serverID = source
-	TriggerClientEvent('heli:light.down', -1, serverID)
-end)
-
-RegisterServerEvent('heli:radius.up')
-AddEventHandler('heli:radius.up', function()
-	local serverID = source
-	TriggerClientEvent('heli:radius.up', -1, serverID)
-end)
-
-RegisterServerEvent('heli:radius.down')
-AddEventHandler('heli:radius.down', function()
-	local serverID = source
-	TriggerClientEvent('heli:radius.down', -1, serverID)
 end)
